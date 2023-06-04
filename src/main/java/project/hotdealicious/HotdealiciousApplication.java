049c9f5b0f3c39372cc308c547858a15b9f521d7
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-04 19:12:15 +0900

    Project Init

---
+++ src/main/java/project/hotdealicious/HotdealiciousApplication.java
@@ -0,0 +1,13 @@
+package project.hotdealicious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotdealiciousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotdealiciousApplication.class, args);
+	}
+
+}
