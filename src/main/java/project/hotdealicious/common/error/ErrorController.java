beff2b791eca0a0a7868675ad88d01a3051e7fde
SUYEON KIM <waterkite94@gmail.com>
2023-06-19 10:45:27 +0900

    [#2] Feat: Add Owner CRUD & 회원가입, 수정, 탈퇴 API

--- src/main/java/project/hotdealicious/common/error/ErrorController.java
+++ src/main/java/project/hotdealicious/common/error/ErrorController.java
@@ -39,4 +39,11 @@ public class ErrorController {
 		HttpRequestMethodNotSupportedException error) {
 		return ErrorApiResult.onError(HttpStatus.BAD_REQUEST.value(), error.getLocalizedMessage());
 	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(RuntimeException.class)
+	public BaseWrapper<Object> handlerSqlIntegrityConstraintViolationException(
+		RuntimeException error) {
+		return BaseWrapper.onFailure(HttpStatus.BAD_REQUEST.value(), error.getLocalizedMessage());
+	}
 }
