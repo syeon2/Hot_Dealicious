a9865fb44b13666cbe479c93068a2767d75666b1
SUYEON KIM <waterkite94@gmail.com>
2023-06-29 21:00:07 +0900

    [#5] Feat: Add WebSocket Connection With Rider

---
+++ src/main/java/project/hotdealicious/rider/dto/GeoLocation.java
@@ -0,0 +1,14 @@
+package project.hotdealicious.rider.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GeoLocation {
+
+	private double x;
+	private double y;
+}
--- src/main/java/project/hotdealicious/rider/web/RiderProfileController.java
+++ src/main/java/project/hotdealicious/rider/web/RiderProfileController.java
@@ -1,7 +1,11 @@
 package project.hotdealicious.rider.web;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import project.hotdealicious.common.util.basewrapper.ApiResult;
+import project.hotdealicious.rider.domain.WorkStatus;
+import project.hotdealicious.rider.dto.RiderGeoDto;
 import project.hotdealicious.rider.dto.SaveRiderDto;
 import project.hotdealicious.rider.dto.UpdateRiderDto;
-import project.hotdealicious.rider.dto.UpdateWorkStatusDto;
 import project.hotdealicious.rider.service.RiderProfileService;
 
 @RestController
@@ -22,6 +27,8 @@ import project.hotdealicious.rider.service.RiderProfileService;
 public class RiderProfileController {
 
 	private final RiderProfileService riderProfileService;
+	private final RedisTemplate<String, Object> redisGeoTemplate;
+	private final RedisTemplate<String, String> redisRoutingTableTemplate;
 
 	@PostMapping
 	public ApiResult<Long> join(@Valid @RequestBody SaveRiderDto saveRiderDto) {
@@ -35,15 +42,33 @@ public class RiderProfileController {
 		return ApiResult.onSuccess(riderId);
 	}
 
-	@PostMapping("/commute/{id}")
-	public ApiResult<Long> checkCommute(@PathVariable Long id, @RequestBody UpdateWorkStatusDto updateWorkStatusDto) {
-		Long riderId = riderProfileService.updateWorkStatus(id, updateWorkStatusDto.getWorkStatus());
-		return ApiResult.onSuccess(riderId);
-	}
-
 	@DeleteMapping("/{id}")
 	public ApiResult<Long> withdrawRider(@PathVariable Long id) {
 		Long riderId = riderProfileService.withdraw(id);
 		return ApiResult.onSuccess(riderId);
+	}
+
+	@MessageMapping("/commute")
+	public ApiResult<Long> checkCommute(RiderGeoDto riderGeoDto, HttpServletRequest request) {
+		Point point = new Point(riderGeoDto.getData().getX(), riderGeoDto.getData().getY());
+		redisGeoTemplate.opsForGeo().add("myMap", point, riderGeoDto.getRiderId());
+
+		if (riderGeoDto.getWorkStatus() == WorkStatus.INIT) {
+			connectWebSocketServerToRider(riderGeoDto.getRiderId(), request);
+		}
+
+		return ApiResult.onSuccess(riderGeoDto.getRiderId());
+	}
+
+	private void connectWebSocketServerToRider(@PathVariable Long id, HttpServletRequest request) {
+		redisRoutingTableTemplate.opsForValue().set(String.valueOf(id), makeUri(request));
+	}
+
+	private String makeUri(HttpServletRequest request) {
+		String scheme = request.getScheme();
+		String host = request.getServerName();
+		int remotePort = request.getServerPort();
+
+		return scheme.concat("://").concat(host).concat(":").concat(String.valueOf(remotePort));
 	}
 }
